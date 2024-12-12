16e5a001f91195e7df05803f6c97df0667647971
PeterSahlin <145502083+PeterSahlin@users.noreply.github.com>
2024-12-12 16:23:19 +0100

    Add project files.

---
+++ TriviaApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TriviaApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ TriviaApp/Model/TriviaQuestion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaApp.Model
+{
+    public class TriviaQuestion
+    {
+        public string Type { get; set; }
+        public string Difficulty { get; set; }
+        public string Category { get; set; }
+        public string Question { get; set; }
+        public string CorrectAnswer { get; set; }
+        public List<string> IncorrectAnswers { get; set; }
+    }
+}
