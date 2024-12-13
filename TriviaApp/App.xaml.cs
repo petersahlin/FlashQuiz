2c1198bbafede50831774d8307cbbbe15f9342e0
PeterSahlin <145502083+PeterSahlin@users.noreply.github.com>
2024-12-13 15:24:58 +0100

    Function to navigate to question page when button is clicked on main page works.

--- TriviaApp/App.xaml.cs
+++ TriviaApp/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace TriviaApp
+﻿using TriviaApp.Pages;
+
+namespace TriviaApp
 {
     public partial class App : Application
     {
@@ -7,6 +9,8 @@
             InitializeComponent();
 
             MainPage = new AppShell();
+
+           
         }
     }
 }
